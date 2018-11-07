956616ffc732151f6117e400d7c77647ffbb765f
bruno <chavez.mackie.bruno@gmail.com>
2018-11-07 08:56:50 -0500

    update, create

--- app/controllers/api/v1/usuarios_controller.rb
+++ app/controllers/api/v1/usuarios_controller.rb
@@ -1,37 +1,41 @@
 module Api
     module V1
       class UsuariosController < ApplicationController
+
         def index
           usuarios=Usuario.select("usuarios.nombre","usuarios.codigo")
-          render json: {status: 'SUCCESS', message: Articules upload', data: usuarios },status: :ok
+          render json: {status: 'SUCCESS', message: Articules upload', data: usuarios },status: :ok 
 
 
 
         end
+
      
         def create
           alumnos=Usuario.new(articules_params)
           if alumnos.save
-            render json: {status: 'SUCCESS', message:'Saved Articules', data:alumnos},status: :ok
+            render json:  {status: 'SUCCESS', message:'Saved Articules', data:alumnos},status: :ok 
           else
-            render json: {status: 'ERROR', message:'Articules not saved', data:alumnos.errors},status: :unprocessable_entity
+            render json:  {status: 'ERROR', message:'Articules not saved', data:alumnos.errors},status: :unprocessable_entity 
           end
         end
 
-      
+
+
         def update
           alumnos = Usuario.find(params[:id])
           if alumnos.update_attributes(articules_params)
-            render json: {status: 'SUCCESS', message:'Updated Articules', data:alumnos},status: :ok
+            render json:  {status: 'SUCCESS', message:'Updated Articules', data:alumnos},status: :ok 
           else
-            render json: {status: 'ERROR', message:'Articules not updated', data:alumnos.errors},status: :unprocessable_entity
+            render json:  {status: 'ERROR', message:'Articules not updated', data:alumnos.errors},status: :unprocessable_entity 
           end
         end
 
 
+
         private
-        def articules_params
-          params.permit(:nombre,:codigo,:nivelu )
+        def articules_params 
+          params.permit(:nombre,:codigo,:nivelu ) 
 
         end
 
