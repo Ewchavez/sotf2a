e3c05c94127ce8b0f4207ebca9d01e581851d5fc
ewchavez <ewchavez@hotmail.com>
2018-11-19 08:51:15 -0500

    arreglar

--- test/models/usuario_test.rb
+++ test/models/usuario_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UsuarioTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+
+  test "the truth" do
+     assert true
+  end
+
+
+test "should not save article without title" do
+  article = Usuario.new
+  assert_not article.save
+end
+
 end
