85033c4a09cc2d9fab5bfa00c515a338773e8ea7
ewchavez <ewchavez@hotmail.com>
2018-11-21 20:47:03 -0500

    agregar id

--- test/controllers/asesoria_g_controller_test.rb
+++ test/controllers/asesoria_g_controller_test.rb
@@ -2,6 +2,20 @@ require 'test_helper'
 
 class AsesoriaGControllerTest < ActionDispatch::IntegrationTest
 
-  
+  test "Solicitar asesoria" do
+
+  get "/asesoria_g/solicitarase",
+     params: { cursoselec:1 }
+   assert_response :success, "Se ha solicitado la asesoria"
+  end
+
+
+  test "Crear asesoria" do
+
+  get "/asesoria_g/crearasesoria",
+     params: {cursoesco:101,calendario:"otro", calendario2: "Lunes", horai:"16-18",lugar:"w-102"}
+   assert_response :success, "Asesoria creada"
+  end
+
 
 end
